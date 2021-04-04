981f25e62712403dc99072275a2137254bf426e7
ChadDiaz <chadcdiaz@gmail.com>
2021-04-04 18:12:32 -0400

    auth/register and auth/login with bcrypt is working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,12 +4,30 @@ const users = require("../users/users-model.js");
 const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res, next) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10);
-    user.password = hash;
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 16);
+  user.password = hash;
   try {
     const savedUser = await users.add(user);
     res.status(201).json(savedUser);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  let { username, password } = req.body;
+  try {
+    const user = await users.findBy({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ message: `welcome ${user.username}` });
+    } else {
+      const err = new Error();
+      err.statusCode = 401;
+      err.message = "invalid creds.";
+      next(err);
+    }
   } catch (err) {
     console.log(err);
     next(err);
